6fc1d62cec73ef4fdeb695d589df4dd9bdca5e83
Misha <mishagrigoryan@gmail.com>
2024-02-08 23:36:26 -0500

    busted

--- blobConnection.py
+++ blobConnection.py
@@ -1,15 +1,12 @@
-from azure.storage.blob import BlobServiceClient, BlobClient, ContainerClient
+from azure.storage.blob import BlobServiceClient
 import config
-
 import ssl
-
 
 # Custom SSL context
 ssl_context = ssl._create_unverified_context()
 
-connection_string = config.junkString
+connection_string = config.junkString  # Ensure this is your correct connection string
 blob_service_client = BlobServiceClient.from_connection_string(connection_string, connection_ssl_context=ssl_context)
-
 
 container_name = 'crime'
 container_client = blob_service_client.get_container_client(container_name)
@@ -31,4 +28,3 @@ for blob in container_client.list_blobs():
     # Download blob to a local file
     with open(download_file_path, "wb") as download_file:
         download_file.write(blob_client.download_blob().readall())
-
